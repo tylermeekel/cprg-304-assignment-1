b9d51bfeca769729ec0906d76c135f68f44df563
nicdraffin <nic.draffin@gmail.com>
2024-10-15 13:30:47 -0600

    pyramid class complete
    Completed calculation of pyramid class

--- src/shapes/Pyramid.java
+++ src/shapes/Pyramid.java
@@ -1,6 +1,24 @@
 package shapes;
 
-public class Pyramid
-{
+public class Pyramid extends Shape {
+    private double sideLength;
+    public Pyramid(double height, double sideLength) {
+        super(height);
+        this.sideLength = sideLength;
+    }
 
+    @Override
+    public double calcBaseArea() {
+        return Math.pow(sideLength, 2);
+    }
+
+    @Override
+    public double calcVolume() {
+        return (1.0 / 3.0) * calcBaseArea() * getHeight();
+    }
+
+    @Override
+    public String toString() {
+        return "Pyramid with height " + getHeight() + " and base side length " + sideLength;
+    }
 }
