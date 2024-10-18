2ec89fb29226075376941bc605ffda252ed2d526
Jihun Gwak <gjihun1395@gmail.com>
2024-10-18 16:49:00 -0600

    Fix: Comparator

--- src/utilities/BaseAreaComparator.java
+++ src/utilities/BaseAreaComparator.java
@@ -5,16 +5,11 @@ import java.util.Comparator;
 import shapes.Shape;
 
 public class BaseAreaComparator implements Comparator<Shape> {
-
-	public BaseAreaComparator() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public int compare(Shape shape1, Shape shape2) {
-		if(shape1.calcBaseArea() > shape2.calcBaseArea() ) return 1;
-		if(shape1.calcBaseArea() < shape2.calcBaseArea()) return -1;
-		return 0;
+        double area1 = shape1.calcBaseArea();
+        double area2 = shape2.calcBaseArea();
+        return Double.compare(area1, area2);
 	}
 
 }
